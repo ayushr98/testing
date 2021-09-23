2d6700a7fbd74725f88f6c7abb1cd1bab71b715c
Ayush Ranjan <M7424@MICROCLINIC.IN>
2021-09-23 12:56:05 +0530

    work done

--- intro-python/part1/hands_on_exercise.py
+++ intro-python/part1/hands_on_exercise.py
@@ -7,24 +7,41 @@ import random
 
 # TODO: Write a print statement that displays both the type and value of `pi`
 pi = math.pi
+print(math.pi)
+print(type(pi))
 
 
 # TODO: Write a conditional to print out if `i` is less than or greater than 50
 i = random.randint(0, 100)
-
+if i < 50:
+    print("i is less than 50")
+elif i > 50:
+    print ("i is greater than 50")
+else:
+    print ("i is equal to 50") 
+    
 
 # TODO: Write a conditional that prints the color of the picked fruit
 picked_fruit = random.choice(['orange', 'strawberry', 'banana'])
+if picked_fruit == 'orange':
+    print("fruit picked  ", picked_fruit, " orange")
+elif picked_fruit == 'strawberry':
+    print("fruit picked  ", picked_fruit, "  red")
+elif picked_fruit == 'banana':
+    print("fruit picked ", picked_fruit, " yellow")
+
 
 
 # TODO: Write a function that multiplies two numbers and returns the result
 # Define the function here.
-
+def mul_two_num (a,b):
+    c = a*b
+    return c
 
 # TODO: Now call the function a few times to calculate the following answers
 
-print("12 x 96 =",)
+print("12 x 96 =",mul_two_num(12,96))
 
-print("48 x 17 =",)
+print("48 x 17 =",mul_two_num(48,17))
 
-print("196523 x 87323 =",)
+print("196523 x 87323 =",mul_two_num(196523,87323))
